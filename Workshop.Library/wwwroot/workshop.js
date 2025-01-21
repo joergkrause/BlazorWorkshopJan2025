a3423c98a76ce5bb63e10e5abcb5d296ba42104b
Jörg Krause <joerg@joergkrause.de>
2025-01-21 16:24:20 +0100

    JavaScript

---
+++ Workshop.Library/wwwroot/workshop.js
@@ -0,0 +1,11 @@
+﻿window.copyTextToClipboard = (text, element) => {
+  navigator.clipboard.writeText(text)
+    .then(() => {
+      element.innerText = 'Copied!';
+      element.classList.remove('invisible');
+      setTimeout(() => {
+        element.innerText = 'Copy';
+        element.classList.add('invisible');
+      }, 2000);
+    });
+}
