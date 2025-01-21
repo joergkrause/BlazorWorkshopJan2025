d51f073d0ad7a1f0a723e8947dfa8a87925cd3f9
Jörg Krause <joerg@joergkrause.de>
2025-01-21 14:30:01 +0100

    Form Demo Reflection

---
+++ Workshop.ViewModels/Attributes/AccessKeyAttribute.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Workshop.ViewModels.Attributes
+{
+  [AttributeUsage(AttributeTargets.Property)]
+  public class AccessKeyAttribute : Attribute
+  {
+    public char Key { get; set; }
+
+    public AccessKeyAttribute(char key)
+    {
+      Key = key;
+    }
+  }
+}
---
+++ Workshop.ViewModels/ProductEditViewModel.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+using Workshop.ViewModels.Attributes;
+
+namespace Workshop.ViewModels
+{
+  public class ProductEditViewModel
+  {
+    [Required]
+    [StringLength(100)]
+    [Display(Name = "Produktname", Description ="Das ist der Name des Produkts", Order = 100)]
+    [AccessKey('n')]
+    public string Name { get; set; } = default!;
+
+    [Range(0, 1000)]
+    [Display(Name = "Artikelpreis", Description = "Das ist der Preis des Artikels in €", Order = 200)]
+    [DataType(DataType.Currency)]
+    [UIHint("€")]
+    [AccessKey('P')]
+    public int Price { get; set; }
+  }
+}
