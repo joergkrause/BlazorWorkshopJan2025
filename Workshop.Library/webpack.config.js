ee6ef94839848fd12ebae0598ff5585008c40e15
Jörg Krause <joerg@joergkrause.de>
2025-01-24 11:20:44 +0100

    webpack

---
+++ Workshop.Library/webpack.config.js
@@ -0,0 +1,60 @@
+﻿const CopyWebpackPlugin = require('copy-webpack-plugin');
+const TerserPlugin = require('terser-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const path = require('path');
+
+module.exports = {
+  mode: 'production',
+  entry: ['./wwwroot/workshop.js', './src/styles/main.scss'],
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, '/wwwroot/dist')
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: [
+          MiniCssExtractPlugin.loader,
+          {
+            loader: 'css-loader'
+          }
+        ]
+      },
+      {
+        test: /\.scss$/,
+        use: [
+          MiniCssExtractPlugin.loader,
+          {
+            loader: 'css-loader'
+          },
+          'sass-loader'
+        ]
+      },
+      {
+        test: /\.(ttf|woff|woff2|eot|svg)$/,
+        type: 'asset/resource',
+        generator: {
+          filename: 'fonts/[name][ext]'
+        }
+      },
+      {
+        test: /\.(png|jpg|jpeg|gif)$/,
+        type: 'asset/resource',
+        generator: {
+          filename: 'fonts/[name][ext]'
+        }
+      }
+    ]
+  },
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: 'styles.css'
+    }),
+    //new CopyWebpackPlugin({
+    //  patterns: [
+    //    { from: path.resolve(__dirname, '../Deployment/Playwright/'), to: path.resolve(__dirname, 'wwwroot/playwright/') }
+    //  ]
+    //})
+  ]
+};
